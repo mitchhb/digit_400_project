4fc850157c6cbd0d82a2554610e5b08e918d7cbc
Mitchell Ballew <mitchhb12@gmail.com>
2019-05-04 03:15:40 +0000

    Finished (FOR NOW)

--- __init__.py
+++ __init__.py
@@ -23,10 +23,7 @@ def login_required(f):
 APP_CONTENT = Content()
 
 APP_CONTENT = {
-    "Home":[["Chassis Mount Wing", "/chassismountwing/","Check out the popular Chassis Mount Wings for many different cars!", "https://cdn.shopify.com/s/files/1/0919/7894/products/28513873682_3fe6bc8374_o_grande.jpg?v=1512449671"],
-           ["Duckbill Wing", "/duckbillwing", "Check out the different types of Duckbill Wings!"],
-           ["Splitters", "/splitters/", "The latest and best quality splitters!"],
-           ["Body Kits", "/bodykits/", "Check out the best body kits for your car!"]],
+    "Home":[["Chassis Mount Wing", "/chassismountwing/","Check out the popular Chassis Mount Wings for many different cars!", "https://cdn.shopify.com/s/files/1/0919/7894/products/28513873682_3fe6bc8374_o_grande.jpg?v=1512449671"]],
     "Profile":[["User Profile", "/profile/", "Edit your profile here!", "https://cdn.wallpapersafari.com/11/79/xMyfJT.jpg"],
               ["Settings", "/settings/", "App Settins, no biggie."],
               ["Terms of Sercive", "/tos/", "The legal stuff."],],
@@ -149,28 +146,266 @@ def register():
     except Exception as e:
             return(str(e)) # remember to remove! for debugging only!
         
+@app.route('/about/')
+def about():
+    try:
+        return render_template("about.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/companies/')
+def companies():
+    try:
+        return render_template("companies.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/support/')
+def support():
+    try:
+        return render_template("support.html")
+    except Exception as e:
+        return render_template("500.html", error = e)   
+
+        
 @app.route('/chassismountwing/')
 def chassis():
     try:
         return render_template("chassismountwing.html",)
     except Exception as e:
         return render_template("500.html", error = e)
-        
-@app.route('/welcome/')
-def welcome_to_jinja():
+    
+@app.route('/duckbillwing/')
+def duckbill():
     try:
-        #this is where all the python goes:
-        
-        def my_function():
-            output = ["DIGIT 400 is good", "Python, Java, php, SQL, C++","<p><strong>hello world</strong></p>", 42, "42"]
-            return output
-        
-        output = my_function()
-        
-        return render_template("templating_demo.html", output = output)
-        
+        return render_template("duckbillwing.html",)
     except Exception as e:
-        return str(e)
+        return render_template("500.html", error = e)
+    
+@app.route('/trunkmounted/')
+def trunkmount():
+    try:
+        return render_template("trunkmountwing.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/nissan/')
+def chassis350z():
+    try:
+        return render_template("chassisnissan.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/duckbillwing/nissan/')
+def duckbillnissan():
+    try:
+        return render_template("duckbillnissan.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/subaru/')
+def chassissubaru():
+    try:
+        return render_template("chassissubaru.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/duckbillwing/subaru/')
+def duckbillsubaru():
+    try:
+        return render_template("duckbillsubaru.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/duckbillwing/mitsubishi/')
+def duckbillmitsubishi():
+    try:
+        return render_template("duckbillmitsubishi.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/duckbillwing/infiniti/')
+def duckbillinfinti():
+    try:
+        return render_template("duckbillinfinti.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/duckbillwing/hyundai/')
+def duckbillhyundai():
+    try:
+        return render_template("duckbillhyundai.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/duckbillwing/honda/')
+def duckbillhonda():
+    try:
+        return render_template("duckbillhonda.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/duckbillwing/scion/')
+def duckbillscion():
+    try:
+        return render_template("duckbillscion.html",)
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/bmw/')
+def bmw():
+    try:
+        return render_template("chassisbmw.html")
+    except Exception as e:
+        return render_template("500.html", error = e)   
+    
+@app.route('/chassismountwing/chevrolet/')
+def chevy():
+    try:
+        return render_template("chassischevy.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/dodge/')
+def dodge():
+    try:
+        return render_template("chassisdodge.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/ford/')
+def ford():
+    try:
+        return render_template("chassisford.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/honda/')
+def honda():
+    try:
+        return render_template("chassishonda.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/chassismountwing/hyundai/')
+def hyundai():
+    try:
+        return render_template("chassishyundai.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/infiniti/')
+def infiniti():
+    try:
+        return render_template("chassisinfiniti.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/chassismountwing/lexus/')
+def lexus():
+    try:
+        return render_template("chassislexus.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/chassismountwing/mazda/')
+def mazda():
+    try:
+        return render_template("chassismazda.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/scion/')
+def scion():
+    try:
+        return render_template("chassisscion.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/chassismountwing/toyota/')
+def toyota():
+    try:
+        return render_template("chassistoyota.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/trunkmountwing/nissan/')
+def trunknissan():
+    try:
+        return render_template("trunknissan.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/trunkmountwing/subaru/')
+def trunksubaru():
+    try:
+        return render_template("trunksubaru.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/trunkmountwing/mitsubishi/')
+def trunkmitsu():
+    try:
+        return render_template("trunkmitsubishi.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/trunkmountwing/mazda/')
+def trunkmazda():
+    try:
+        return render_template("trunkmazda.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/trunkmountwing/scion/')
+def trunkscion():
+    try:
+        return render_template("trunkscion.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/trunkmountwing/toyota/')
+def trunktoyota():
+    try:
+        return render_template("trunktoyota.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/bodykits/')
+def bodykits():
+    try:
+        return render_template("bodykits.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/diffusers/')
+def diffusers():
+    try:
+        return render_template("diffusers.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+@app.route('/splitters/')
+def splitters():
+    try:
+        return render_template("splitters.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+
+@app.route('/decals/')
+def decals():
+    try:
+        return render_template("decals.html")
+    except Exception as e:
+        return render_template("500.html", error = e)
+    
+    
+@app.route('/welcome/')
+def welcome():
+    try:
+        return render_template("welcome.html")
+    except Exception as e:
+        return render_template("500.html", error = e) 
 
         
 #sitemap
